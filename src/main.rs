dbdbbaee38471fb79287d065b0c33554aee97cb2
Roque Santos <roque-100@hotmail.com>
2024-11-09 02:35:27 -0300

    added math::sign functions and docs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use std::ops::Add;
-use vector2::Vector2;
+use vector2::{Vector2};
 
 mod math;
 mod vector2;
 
 fn main() {
-    let vec1 = Vector2::new(50, 50);
-    let vec2 = Vector2::new(10, 20);
-
+    
     // math functions
     println!("|-10| = {}", math::abs(-10.0));
-
+    println!("sign of -23 = {}, sign of 42 = {}, sign of 0 = {}", math::sign(-23.0), math::sign(42.0), math::sign(0.0));
+    
     // vector2 debug
+    // let vec1 = Vector2::new(50, 50);
+    // let vec2 = Vector2::new(10, 20);
+
     // println!("vec1 = {}", vec1.to_string());
     // println!("vec2 = {}", vec2.to_string());
     // println!("vec1 + vec2 = {}", (vec1 + vec2).to_string());
--- src/math.rs
+++ src/math.rs
@@ -1,7 +1,62 @@
+
+/// Returns the absolute value of a f32 number
+/// 
+/// ### Examples
+/// ```
+/// math::abs(-1.2) -> 1.2
+/// math::abs(1.0) -> 1.0
+/// math::abs(-32.345) -> 32.345
+/// ```
 pub fn abs(n: f32) -> f32 {
-    if (n < 0.0) {
+    if n < 0.0 {
         return n * -1.0;
     }
 
     return n;
+}
+
+/// Returns the absolute value of an i32 number
+/// 
+/// ### Examples
+/// ```
+/// math::absi(-1) -> 1
+/// math::absi(1) -> 1
+/// math::absi(-32) -> 32
+/// ```
+pub fn absi(n: i32) -> i32 {
+    if n < 0 {
+        return n * -1;
+    }
+
+    return n;
+}
+
+/// Returns the sign of a f32 number
+/// 
+/// ### Examples
+/// ```
+/// math::sign(-3.0) -> -1.0
+/// math::sign(0.0) -> 0.0
+/// math::sign(7.0) -> 1.0
+/// ```
+pub fn sign(n: f32) -> f32 {
+    return 
+        if n < 0.0 { -1.0 }
+        else if n > 0.0 { 1.0 }
+        else { 0.0 }
+}
+
+/// Returns the sign of an i32 number
+/// 
+/// ### Examples
+/// ```
+/// math::sign(-3) -> -1
+/// math::sign(0) -> 0
+/// math::sign(7) -> 1
+/// ```
+pub fn signi(n: i32) -> i32 {
+    return 
+        if n < 0 { -1 }
+        else if n > 0 { 1 }
+        else { 0 }
 }
